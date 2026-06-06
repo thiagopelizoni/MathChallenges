6341e92ff9e50c55bf8a1c4333a5b4decde38425
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2026-06-06 20:15:26 -0300

    refactor: use sympy primes in problem 293

--- src/problem_293.py
+++ src/problem_293.py
@@ -1,33 +1,10 @@
 # Problem 293: https://projecteuler.net/problem=293
 
-from math import isqrt
+from sympy import isprime, primerange
 
 
 LIM = 10**9
-
-
-def prime_list(n):
-    s = bytearray(b"\x01") * (n + 1)
-    s[:2] = b"\x00\x00"
-
-    for p in range(2, isqrt(n) + 1):
-        if s[p]:
-            s[p * p:n + 1:p] = b"\x00" * ((n - p * p) // p + 1)
-
-    return [p for p in range(n + 1) if s[p]]
-
-
-primes = prime_list(100_000)
-
-
-def is_prime(n):
-    for p in primes:
-        if p * p > n:
-            return n > 1
-        if n % p == 0:
-            return n == p
-
-    return True
+primes = list(primerange(2, 30))
 
 
 def admissible(i, n, xs):
@@ -43,7 +20,7 @@ def admissible(i, n, xs):
 
 def fortunate(n):
     m = 3
-    while not is_prime(n + m):
+    while not isprime(n + m):
         m += 2
     return m
 
